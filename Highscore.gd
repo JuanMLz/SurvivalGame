90bbc96b69bf07fa6438c509a4c7a190533fde29
Juan Moraes Lopes <139184458+JuanMLz@users.noreply.github.com>
2024-11-05 21:42:23 -0300

    Update Highscore.gd

--- Highscore.gd
+++ Highscore.gd
@@ -1,8 +1,10 @@
 extends Label
 
+# Exibe a pontuação mais alta
 func _ready():
 	text = str(Global.highscore)
-	
+
+# Atualiza o highscore quando a pontuação atual ultrapassa o recorde
 func _process(_delta):
 	if Global.points > Global.highscore:
 		Global.highscore = Global.points
